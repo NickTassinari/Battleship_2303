ecc68b6caaca83ba1d96547c762b38f416a85495
lorenzo rivera <lorenzo23rivera@gmail.com>
2023-04-06 16:52:58 -0700

    Add health and sunk code

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Ship do
       expect(cruiser.length).to eq(3)
     end
   end
-end 
+
+  describe '#health' do
+    it 'has health' do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.health).to eq(3)
+    end
+    it '#sunk?' do
+      cruiser = Ship.new("Cruiser", 3)
+      expect(cruiser.sunk?).to eq(false)
+      cruiser.hit
+      expect(cruiser.health).to eq(2)
+      cruiser.hit
+      expect(cruiser.health).to eq(1)
+      expect(cruiser.sunk?).to eq(false)
+      cruiser.hit
+      expect(cruiser.sunk?).to eq(true)
+    end
+  end
+end

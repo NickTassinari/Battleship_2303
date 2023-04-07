09e0cf5107db7bdccb4fabf9189ef45d0362c100
lorenzo rivera <lorenzo23rivera@gmail.com>
2023-04-06 17:36:15 -0700

    Add ship location

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,18 @@
 class Cell 
-  attr_reader :coordinate
-  
+  attr_reader :coordinate, :empty, :ship
+
   def initialize(coordinate)
     @coordinate = coordinate 
+    @ship = nil
+    @empty = true
+  end
+  def empty?
+    @empty
+  end
+  def place_ship(ship)
+    if empty? == true
+      @ship = ship
+      @empty = false
+    end
   end
 end

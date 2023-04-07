a912a698e7d9b47bf7e1812f9eea7bd18edf9171
Nick Tassinari <ntass7800@gmail.com>
2023-04-07 14:54:54 -0600

    add render method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,12 @@
 class Cell 
-  attr_reader :coordinate, :empty, :ship
+  attr_reader :coordinate, :empty, :ship, :render, :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate 
     @ship = nil
     @empty = true
+    @rendered = "."
+    @fired_upon = false 
   end
   def empty?
     @empty
@@ -15,4 +17,22 @@ class Cell
       @empty = false
     end
   end
+  
+  def fire_upon 
+    @fired_upon = true 
+  end 
+  
+  def render
+   if @fired_upon == true && empty == true 
+     @rendered = "M"
+    elsif @fired_upon == true && empty == false 
+      @rendered = "H"
+    elsif @fired_upon == false && empty == false 
+      @rendered = "S"
+    else 
+      @rendered
+   end
+  end
+
+
 end
